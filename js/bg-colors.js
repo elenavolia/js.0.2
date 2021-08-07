2d0f0ed5f56131576e37c241fc7243dd5a80ec53
Elena <66116069+elenavolia@users.noreply.github.com>
2021-08-07 12:24:52 +0300

    colours for remdomaise

--- js/bg-colors.js
+++ js/bg-colors.js
@@ -1,6 +1,6 @@
 //рэндомно меняющийся фон
 
-const colors = ['teal', 'tomato', 'orange', 'green', 'deeppink', 'skyblue', 'navy'];
+const colors = ['teal', 'tomato', 'orange', 'green', 'deeppink', 'skyblue', 'navy', 'blue', 'yellow', 'peach', 'gold', 'pink', 'maroon', 'salmon', 'rosybrown', 'brown', 'ivory', 'silver', 'red', 'purple', 'violet', 'scarlet', 'amber', 'beige', 'lilac', 'azure', 'apricot', 'aqua', 'aquamarine', 'berry', 'burgundy', 'cambridgeblue', 'cerise', 'charcoal', 'chartreuse', 'chlorine', 'chocolate', 'cocoa', 'coral', 'cornflower', 'cornsilk', 'cream', 'cyan', 'emerald', 'fallow', 'forest', 'fuchsia', 'garnet', 'indigo', 'jade', 'lavender', 'lemon', 'lime', 'magenta', 'mauve', 'mazarine', 'mint', 'murrey', 'orangey', 'pastel', 'petunia', 'pistachio', 'plum', 'pumpkin', 'raspberry', 'rose', 'sand', 'seagreen', 'seafoam', 'slate', 'tan', 'turquoise', 'wheat'] ;
 const max = colors.length - 1;
 const min = 0;
 
